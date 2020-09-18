961c5b571de740cf68cdabd717d1ff623fbfbed3
Dodojon <asdever0012@gmail.com>
2020-09-18 16:19:04 +0400

    implement timer

--- src/App.js
+++ src/App.js
@@ -1,24 +1,36 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import './App.scss';
+
+const sections = [
+  1, 2, 3, 4, 5, 6, 7, 8, 9, 10,
+  11, 12, 13, 14, 15, 16, 17, 18, 19, 20,
+  21, 22, 23, 24, 25, 26, 27, 28, 29, 30
+];
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {/* <video id="background-video" loop autoPlay>
+        <source src="https://www.youtube.com/watch?v=ixboYwEqJS8" type="video/mp4" />
+          Your browser does not support the video tag.
+      </video> */}
+      <div className="clock">
+        <div className="no no--min-tens no--sec-tens">
+          {sections.map(section => <div key={`min-tens-${section}`} />)}
+        </div>
+        <div className="no no--min-units">
+          {sections.map(section => <div key={`min-units-${section}`} />)}
+        </div>
+        <div className="no no--colon">
+          {sections.map(section => <div key={`colon-${section}`} />)}
+        </div>
+        <div className="no no--sec-tens">
+          {sections.map(section => <div key={`sec-tens-${section}`} />)}
+        </div>
+        <div className="no no--sec-units">
+          {sections.map(section => <div key={`sec-tens-${section}`} />)}
+        </div>
+      </div>
     </div>
   );
 }
