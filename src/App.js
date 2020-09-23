de85e2a4dc7ba20a6ca3262d3e24d7c449180447
Webdev <asdever0012@gmail.com>
2020-09-23 04:32:03 +0300

    chage meta info and deadline

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import './flipdown.css';
 
 function App() {
   useEffect(() => {
-    const untilDate = new Date("2020-09-26T00:00:00.000Z").getTime() / 1000;
+    const untilDate = new Date("2020-10-03T00:00:00.000Z").getTime() / 1000;
 
     new FlipDown(untilDate)
       .start() // Start the countdown
